a7bcd8f82471dbff3bce55dec9d6b76b642fe40e
ahjee12 <ahjee200@gmail.com>
2022-10-06 23:13:09 +0900

    2022.10.06 vue todoList

---
+++ vue-todoList/src/components/ListItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <li>
+    {{ text }}
+    <!-- react에서는 하위에서 상위 component delete method를 전달 -->
+    <!-- vue에서는 상단  -->
+    <button @click="itemDelete">삭제</button>
+  </li>
+</template>
+<script>
+export default {
+  props: {
+    index: {
+      type: Number,
+    },
+    text: {
+      type: String,
+      required: true,
+      //   prop이 undefined로 전달될 때
+      default: "없음!",
+    },
+  },
+  methods: {
+    itemDelete() {
+      this.$emit("delete", this.index);
+    },
+  },
+};
+</script>
+<style lang=""></style>
---
+++ vue-todoList/src/components/TodoList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <!-- react는 useState, e.target or ref onChange함수 필요-->
+    <input type="text" v-model="inputValue" />
+    <!-- react는 onClick={} 중괄호에 method 넣음-->
+    <button @click="addItem">추가하기</button>
+    <ul>
+      <!-- : 앞에 한 칸 띄우기/ 변수 넣을 땐 쌍점, inline일 땐 기본 html 방식과 같음-->
+      <ListItem
+        v-for="(value, i) in listArray"
+        :key="i"
+        :index="i"
+        :text="sendMsg(value)"
+        @delete="catchDeleteItem"
+      />
+    </ul>
+  </div>
+</template>
+<script>
+import ListItem from "./ListItem";
+export default {
+  data() {
+    return {
+      inputValue: "",
+      listArray: [],
+      style: { color: "red" },
+    };
+  },
+  components: {
+    ListItem,
+  },
+  methods: {
+    addItem() {
+      this.listArray.push(this.inputValue);
+      this.inputValue = "";
+    },
+    sendMsg(value) {
+      return value === "" ? undefined : value;
+    },
+    catchDeleteItem(i) {
+      console.log(i);
+      //   this.listArray.splice(undefined, 1);
+      this.listArray.splice(i, 1);
+    },
+  },
+};
+</script>
+<style lang=""></style>
